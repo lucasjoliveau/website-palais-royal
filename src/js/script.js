207daa88afe96bdb852385c6251d850a09adf455
lucas <joliveau.lucas@gmail.com>
2020-09-05 08:33:20 -0400

    Add navigation menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    const selectElement = (element) => document.querySelector(element);
 
+    selectElement('.mobile-menu').addEventListener('click', () => {
+        selectElement('header').classList.toggle('active');
+        selectElement('.background-bg').classList.toggle('active-bg');
+    });
 });
