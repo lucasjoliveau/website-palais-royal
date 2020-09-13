7b2d13bbfba16574010833c1d8d4b7ad5b597516
lucas <joliveau.lucas@gmail.com>
2020-09-13 08:28:37 -0400

    Add close button

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     const selectElement = (element) => document.querySelector(element);
 
+    // Navigation menu
     selectElement('.mobile-menu').addEventListener('click', () => {
         selectElement('header').classList.toggle('active');
         selectElement('.background-bg').classList.toggle('active-bg');
@@ -10,5 +11,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     selectElement('.language-selector-link').addEventListener('click', () => {
        console.log('Click menu langue');
        selectElement('.language-selector-modale').classList.remove('language-selector-modale-hide');
+       selectElement('body').style.overflow = "hidden";
     });
+
+    selectElement('.modale-close').addEventListener('click', () => {
+        selectElement('.language-selector-modale').classList.add('language-selector-modale-hide')
+        selectElement('body').style.overflow = "";
+    })
 });
