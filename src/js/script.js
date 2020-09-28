cfd90b4f89f98efff71315f01ca4da2b3ae7006d
lucas <joliveau.lucas@gmail.com>
2020-09-28 09:54:25 -0400

    Add animations for all pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     AOS.init();
-
     const selectElement = (element) => document.querySelector(element);
 
     // Navigation menu
